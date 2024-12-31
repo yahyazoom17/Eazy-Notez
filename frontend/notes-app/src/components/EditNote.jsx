baf03843c16a9b720e83963762c6da938a5a9e77
Yahya <161343739+yahyazoom17@users.noreply.github.com>
2024-12-31 10:02:03 +0530

    Update EditNote.jsx

--- frontend/notes-app/src/components/EditNote.jsx
+++ frontend/notes-app/src/components/EditNote.jsx
@@ -47,12 +47,12 @@ function EditNote({ onButtonClick, openParam, note }) {
       .then(function (response) {
         toast(response.data.message);
         console.log(response);
+        setTimeout(window.location.reload(), 8000);
       })
       .catch(function (error) {
         console.log(error);
       });
     onButtonClick();
-    window.location.reload();
   };
 
   return (
